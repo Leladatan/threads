01f965d77aff82bacc9333d0e36bc16f86d03473
Leladatan <vov4ik.samurai@yandex.ru>
2023-08-25 14:40:35 +0300

    ref middleware and added pages auth + refactor name path

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,10 +1,11 @@
 import {type FC} from 'react';
+import {SignIn} from "@clerk/nextjs";
 
 const SignInPage: FC = () => {
     return (
-        <div>
-
-        </div>
+        <>
+            <SignIn />
+        </>
     );
 };
 
--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,10 +1,11 @@
 import {type FC} from 'react';
+import {SignUp} from "@clerk/nextjs";
 
 const SignUpPage: FC = () => {
     return (
-        <div>
-
-        </div>
+        <>
+            <SignUp />
+        </>
     );
 };
 
