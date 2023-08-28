52a98e421845da24621c4792791f0ae98f0850e4
Leladatan <vov4ik.samurai@yandex.ru>
2023-08-28 17:26:07 +0300

    added header

--- components/header.tsx
+++ components/header.tsx
@@ -1,6 +1,8 @@
 import {type FC} from 'react';
 import Link from "next/link";
 import Image from "next/image";
+import {OrganizationSwitcher, SignedIn, SignOutButton} from "@clerk/nextjs";
+import {FiLogOut} from "react-icons/fi";
 
 const Header: FC = () => {
     return (
@@ -14,10 +16,27 @@ const Header: FC = () => {
                         width={28}
                         height={28}
                     />
-                    <p className="text-white max-xs:hidden">
+                    <p className="text-white text-3xl font-bold max-md:hidden">
                         Threads
                     </p>
                 </Link>
+
+                <div className="flex items-center gap-1">
+                    <div className="block md:hidden">
+                        <SignedIn>
+                            <SignOutButton>
+                                <FiLogOut className="text-white cursor-pointer" size={24}/>
+                            </SignOutButton>
+                        </SignedIn>
+                    </div>
+                    <OrganizationSwitcher
+                        appearance={{
+                            elements: {
+                                organizationSwitcherTrigger: "py-2 px-4 text-white truncate w-[150px] md:w-[200px] lg:w-[300px]"
+                            }
+                        }}
+                    />
+                </div>
             </nav>
         </header>
     );
